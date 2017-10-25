d8e2403be0f8b34b7171891bb174b6a53f42b916
Sushma Maddirala <sushma.maddirala@wavemaker.com>
2017-10-25 09:34:31 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "testVCS_shareproject",
+  "homePage" : "Main",
+  "name" : "testVCS_shareproject",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
